fd50bdbc4ebe06ea735e0e3196633c262e201a73
HARI VYKUNTA KISHORE ABBINA <159673609+harikishoreabbina@users.noreply.github.com>
2026-04-14 18:11:33 -0500

    Delete File_handeling.py

---
+++ csv_handling.py
@@ -0,0 +1,21 @@
+#CSV Files
+# CSV = Comma Seperated Values, it is the most commanly used file format used to store data
+
+import csv
+
+with open("Students.csv","r") as file:
+    reader = csv.DictReader(file)  # csv.DictReader makes each row as dictionary with column header as keys
+    for i in reader:  ## the for loop will print the every row 
+        print(i)
+        
+        
+        
+with open("Cities.csv","r") as file2:
+    reader2 = csv.DictReader(file2)
+    for i in reader2:
+        print(i)
+        print(i["name"])  # printing only name coloumn  in the file 
+        print(i["column_id"]) # printing the coloumn_id below the name
+        break
+    
+# the output is in Dictionary format {'key' : 'value'}
